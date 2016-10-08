bae956e487dc0d2b3f93a5b688c1d9f458b6572e
Adrien Chinour <adrienchinour@gmail.com>
2016-10-08 23:54:15 +0200

    Affiche dans le terminal une surface de jeu avec des noeuds (position aléatoire et degré aléatoire)

---
+++ Makefile
@@ -0,0 +1,16 @@
+OBJETS = game.o node.o
+CFLAGS = -Wall -std=c99
+LDFLAGS = -L. -lhashi
+EXEC = hashi_text
+CC = gcc
+
+all: hashi_text libhashi.a
+
+hashi_text: hashi_text.c libhashi.a
+	$(CC) $(CFLAGS) -o $@ $< $(LDFLAGS) 
+
+libhashi.a: $(OBJETS)
+	ar rcs $@ $^
+
+clean:
+	rm -f *.a *~ 
---
+++ hashi_text.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#include "node.h"
+#include "game.h"
+
+//#define size 20// dimension de la partie
+
+
+
+int main(int argc ,char *argv[]) {
+   
+	printf("\n");
+
+	if(argc != 3){
+		printf("Erreur dans le nombre d'argument.\nSynthaxe : ./hashi_text TAILLE NBNODE\n");
+		return EXIT_FAILURE;
+	}
+
+	int size = atoi(argv[1]);
+
+   	int nbnode = atoi(argv[2]);
+   
+   	/* Creation d'un tableau de nodes random de size nbnode */
+   	node t[nbnode];
+
+   	srand(time(NULL));
+
+   	for(int i = 0; i < nbnode; i++){
+   		int r_x = rand()% size;
+   		int r_y = rand()% size;
+   		int r_d = rand()% 8 + 1;
+   		t[i]=new_node(r_x , r_y , r_d);
+   	}
+
+   	/* Creation d'une partie à partir des nodes precedement crees */
+   	new_game (nbnode,t);
+
+   	/* Affichage des nodes*/
+
+   	printf("Affichage d'une partie de taille %dx%d:\n",size,size);
+
+	for(int i = 0; i < size; i++){
+
+		for(int j = 0; j < size; j++){
+
+			bool nodeOk = false; //pour savoir si un node a était détecté
+
+			for(int k = 0; k < nbnode; k++){
+
+				if(get_y(t[k]) == j && get_x(t[k]) == i){
+					printf(" %d ",get_required_degree(t[k]));
+					nodeOk = true;
+				}
+
+			}
+
+			if(!nodeOk)
+				printf("   ");
+
+		}
+		printf("|\n");
+	}
+
+	printf("-- fin de la zone de jeu --\n");
+
+	return EXIT_SUCCESS;
+}
