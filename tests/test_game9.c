55a1c3a2e2be236dc7c78ad5372ac2875eee28ca
camille <camille.meyrignac@etu.u-bordeaux.fr>
2016-12-07 15:11:20 +0100

    test9 incomplet

---
+++ tests/test_game9.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "../include/game.h"
+#include "test_toolbox.h"
+
+
+#define EXAMPLE_NB_NODE 8
+#define NB_BRIDGES_MAX 2
+#define NB_DIR 8
+
+int vals[EXAMPLE_NB_NODE][3] = {{0,0,3},{0,2,2},{0,4,2},{2,0,2},{2,2,5},{4,0,1},{4,2,1},{4,4,2}};
+
+static game default_game (){
+    node nodes[EXAMPLE_NB_NODE];
+    for (int i = 0 ; i < EXAMPLE_NB_NODE; i++)
+      nodes[i] = new_node(vals[i][0],vals[i][1],vals[i][2]);
+    game g = new_game(EXAMPLE_NB_NODE, nodes,NB_BRIDGES_MAX,NB_DIR);
+    for (int i = 0 ; i < EXAMPLE_NB_NODE; i++)
+      delete_node(nodes[i]);
+    return g;
+}
+
+bool test_copy_game_bridges(){
+   game g=default_game();
+   bool r=true;
+   add_bridge_dir(g, 0, EAST);
+   add_bridge_dir(g, 0, EAST);
+   add_bridge_dir(g, 0, NORTH);
+   add_bridge_dir(g, 1, EAST);
+   //add_bridge_dir(g, 2, SE);
+   //add_bridge_dir(g, 2, SE);
+
+   add_bridge_dir(g, 4, NE);
+   add_bridge_dir(g, 4, SE);
+   add_bridge_dir(g, 7, SOUTH);
+   //r=game_over(g);
+   delete_game(g);
+   //delete_game(g_copy);
+   return r;
+}
+
+
+
+
+
+
+
+int main (int argc, char *argv[]){
+   bool result = true;
+   result = result && test_copy_game_bridges();
+    if (result){
+       fprintf(stdout,"test9 success\n");
+        return EXIT_SUCCESS;
+    }
+    else{
+       fprintf(stderr,"test9 failure\n");
+       return EXIT_FAILURE;
+    }
+}
