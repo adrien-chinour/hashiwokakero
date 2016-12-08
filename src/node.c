e048c014731f7e74af61a9494e0afe42dcb7fda9
Adrien Chinour <adrienchinour@gmail.com>
2016-12-08 11:55:51 +0100

    The V2 le retour

--- src/node.c
+++ src/node.c
@@ -12,7 +12,7 @@ typedef struct node_s {
 
 node new_node(int x, int y, int required_degree){
 	node n = (node) malloc(sizeof(struct node_s));
-	assert(new != NULL);
+	assert(n != NULL);
 	n->x = x;
 	n->y = y;
 	n->required_degree = required_degree;
