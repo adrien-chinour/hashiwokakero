12f831bc7a24f9d5f1605f35b62b17fccb8306de
louis-gabriel <louis.gabriel.barrere@gmail.com>
2017-02-03 12:19:13 +0100

    ajout d'un test annexe pour le fichier secure

---
+++ src/test/test_secure.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "../core/secure.h"
+#include "test_toolbox.h"
+
+bool lecture() {
+    bool r = true;
+    
+    r = r && test_equality_bool(true, read("test 1", 10), "test 1 : chaine courte");
+    r = r && test_equality_bool(true, read("test 2\n", 10), "test 2 : chaine courte avec retour");
+    r = r && test_equality_bool(false, read("test 3\n", 3), "test 3 : chaine longue");
+    
+    return r;
+}
+
+int main ()
+{
+   printf("%d\n", lecture());
+   return EXIT_SUCCESS;;
+}
