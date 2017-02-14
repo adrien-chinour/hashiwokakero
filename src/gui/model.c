bff2999a8d447b480933b5f2663f5d78540cd5bd
Adrien Chinour <adrienchinour@gmail.com>
2017-02-14 11:17:09 +0100

    ajout de l'affichage des nodes

--- src/gui/model.c
+++ src/gui/model.c
@@ -22,6 +22,7 @@ struct Env_t {
   SDL_Texture ** degree;
   SDL_Texture ** back;
   int max;
+  int nb_nodes;
 }; 
      
 Env * init(SDL_Window* win, SDL_Renderer* ren, int argc, char* argv[]) {
@@ -34,7 +35,8 @@ Env * init(SDL_Window* win, SDL_Renderer* ren, int argc, char* argv[]) {
   int w, h;
   SDL_GetWindowSize(win, &w, &h);
   int max = 0;
-  int nb_nodes = game_nb_nodes(g);
+  int nb_nodes= game_nb_nodes(g);
+
   
   env->x = malloc(sizeof(int)*nb_nodes);
   if(env->x == NULL) {free(env); return NULL;}
@@ -48,12 +50,17 @@ Env * init(SDL_Window* win, SDL_Renderer* ren, int argc, char* argv[]) {
   env->back = malloc(sizeof(SDL_Texture*)*nb_nodes);
   if(env->back == NULL) {free(env); return NULL;}
   
-  for(int i = 0 ; i < game_nb_nodes(g) ; i++){
+  for(int i = 0 ; i < nb_nodes ; i++){
      node n = game_node(g, i);
+     env->x[i] = get_x(n);
+     env->y[i] = get_y(n);
      if(get_x(n) > max) max = get_x(n);
      if(get_y(n) > max) max = get_y(n);
   }
+  
   env->max = max + 1;
+  env->nb_nodes = nb_nodes;
+
   
   delete_game(g);
   return env;
@@ -70,6 +77,18 @@ void render(SDL_Window* win, SDL_Renderer* ren, Env * env) {
   for(int i = 1; i < env->max * 2; i++){
     SDL_RenderDrawLine(ren, (i * size), 0, (i * size), width);
     SDL_RenderDrawLine(ren, 0, (i * size), height, (i * size));
+  }
+
+  /* nodes in black */
+  SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
+  for(int i = 0; i < env->nb_nodes; i++){
+    SDL_Rect srcrect;
+    srcrect.x = (env->x[i] * 2 + 1) * size  - size / 2;
+    srcrect.y = (env->y[i] * 2 + 1) * size - size / 2;
+    srcrect.w = size;
+    srcrect.h = size;
+    SDL_RenderFillRect(ren, &(srcrect));
+    SDL_RenderDrawRect(ren, &(srcrect));
   }
   
 }
