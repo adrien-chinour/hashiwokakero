6860baaf6cf41a6281cfb5671d9b6aa165927a3e
louis-gabriel <louis.gabriel.barrere@gmail.com>
2017-03-03 12:25:33 +0100

    correction plus propre

--- solver/solver.c
+++ solver/solver.c
@@ -9,26 +9,30 @@
 #include "../src/core/file.h"
 
 //cette fonction va renvoyer un tableau contenant la solution du jeu
-void solve(cgame g, int num)
+void solve(game g)
 {
-  //on cherche à poser un pont dès que possible
-  for(int i = 0; i < game_nb_dir(g); i++)
+  //on regarde chaque node pour poser un pont
+  for(int i = 0; i < game_nb_nodes(g); i++)
     {
-      //on voit les caractéristiques des voisins pour poser un pont
-      int neighbour = get_neighbour_dir(g, num, i), d_neighbour = get_degree(g, neighbour);
-      
-      //si un voisin est détecté, qu'il n'y a pas d'obstacle et que l'on peut poser un pont, on le fait
-      if(neighbour != -1 && can_add_bridge_dir(g, num, i) && d_neighbour != r_d_neighbour)
+      //on cherche à poser un pont dès que possible
+      for(int j = 0; j < game_nb_dir(g); j++)
 	{
-	  while(get_degree_dir(g, num, i) < game_nb_max_bridges)
+	  //on voit les caractéristiques des voisins pour poser un pont
+	  int neighbour = get_neighbour_dir(g, i, j), d_neighbour = get_degree(g, neighbour);
+	  
+	  //si un voisin est détecté, qu'il n'y a pas d'obstacle et que l'on peut poser un pont, on le fait
+	  if(neighbour != -1 && can_add_bridge_dir(g, i, j) && d_neighbour != r_d_neighbour)
 	    {
-	      if(can_add_bridge_dir(g, num, i))
+	      while(get_degree_dir(g, i, j) < game_nb_max_bridges)
 		{
-		  add_bridge_dir(g, num, i);
-		  solve(g, num);
+		  if(can_add_bridge_dir(g, i, j))
+		    {
+		      add_bridge_dir(g, i, j);
+		      solve(g);
+		    }
 		}
 	    }
-	}	 
+	}
     }
 }
 
@@ -38,7 +42,7 @@ int main(int argc, char *argv[])
   while(!game_over(g))
     {
       //on résout une grille donnée
-      solve(g, num);
+      solve(g);
     }
   char * save;
   sprintf(save, "%s.solved",argv[1]);
