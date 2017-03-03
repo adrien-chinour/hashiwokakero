27c5ef88cd42f545b3c8399e4712a41b847c46b5
louis-gabriel <louis.gabriel.barrere@gmail.com>
2017-03-03 12:05:22 +0100

    modification solver

--- solver/solver.c
+++ solver/solver.c
@@ -8,23 +8,23 @@
 #include "src/core/game.h"
 
 //cette fonction va renvoyer un tableau contenant la solution du jeu
-int solve(int num, cgame g)
+void solve(cgame g, int num)
 {
   //on cherche à poser un pont dès que possible
   for(int i = 0; i < game_nb_dir(g); i++)
     {
       //on voit les caractéristiques des voisins pour poser un pont
-      int voisin = get_neighbour_dir(g, num, i), d_voisin = get_degree(g, voisin);
+      int neighbour = get_neighbour_dir(g, num, i), d_neighbour = get_degree(g, neighbour);
       
       //si un voisin est détecté, qu'il n'y a pas d'obstacle et que l'on peut poser un pont, on le fait
-      if(voisin != -1 && can_add_bridge_dir(g, num, i) && d_voisin != r_d_voisin)
+      if(neighbour != -1 && can_add_bridge_dir(g, num, i) && d_neighbour != r_d_neighbour)
 	{
 	  while(get_degree_dir(g, num, i) < game_nb_max_bridges)
 	    {
 	      if(can_add_bridge_dir(g, num, i))
 		{
 		  add_bridge_dir(g, num, i);
-		  return solve(num, g);
+		  solve(g, num);
 		}
 	    }
 	}	 
@@ -33,12 +33,10 @@ int solve(int num, cgame g)
 
 int main(int argc, char *argv[])
 {
-  int num = 0;
-  
   while(!game_over(g))
     {
       //on résout une grille donnée
-      num = solve(num, game);
+      solve(game, 0);
     }
   printf("solution found !\n");
   
