14227b60239ec2d9157dcef1ff667b409b041160
Adrien Chinour <adrienchinour@gmail.com>
2017-03-05 17:13:20 +0100

    ajout commentaire pour le fonctionnement du solver

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -27,15 +27,36 @@ game apply_possibility(int * tab, game g, int node_num){
 }
 
 /*
+  Fonction utilisée par create_possibilities pour générer les variantes de tab
+  La fonction génère la variante du tableau dans l'ordre lexicographique
+  Plus de détail : https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+  Le probleme est les doublons générés lorsque deux nombres sont identiques...
+*/
+static int * permutation (int * tab){
+  int * next_tab = malloc(sizeof(*tab));  
+  /*generate next_tab*/
+  return next_tab;
+}
+
+/*
   Retourne une SList
   chaque élément de la liste contient un tableau qui caractérise une possibilté
-  exemeple : [1;2;0;0] -> Un pont au nord et deux au sud
-  Amélioration : 
-  Que les possibilité valide du point de vue de can_add_bridge_dir et get_required_degree
+  exemeple : [1;2;0;0] -> Un pont au nord et deux a l'ouest
  */
 SList create_possibilities(game g, int node_num){
   SList list = slist_create_empty();
-  /* Générer toute les combinaisons possibles */
+  /* Générer toute les combinaisons possibles et les stocker dans la liste */
+
+  /*
+    Procédure :
+     - Comparer le degré requis avec le nombre de pont maximum.
+     - En fonction de ça faire une première répartition des points avec un degré maximum dans la répartition à 1, 2 ...
+     - Faire une permutation de cette combinaison pour obtenir toute les autres
+
+    Exemple : Si on a un degré requis de 4, un nombre de pont maximum à 2 et 4 directions possible sur 4 alors : 
+     -On a la combinaison [1 1 1 1] avec aucune variante. (en faite si avec la fonction permutation lexicographique...)
+     -La combinaison [2 1 1 0] et les variantes [1 2 1 0] , [0 1 2 1] ...
+*/
   return list;
 }
 
@@ -47,7 +68,6 @@ game solve (game g){
 
   SList * combinaison = malloc(sizeof(SList)*game_nb_nodes(g));
   SList * start = malloc(sizeof(SList)*game_nb_nodes(g));
-
   /* 
      On alloue les tableaux de possibilités 
      et on applique les premières possibilités sur chaque noeud 
