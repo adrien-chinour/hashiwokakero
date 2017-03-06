407d6d5a146eeb20a28480c392a1d821a52ccd38
Adrien Chinour <adrienchinour@gmail.com>
2017-03-06 20:17:11 +0100

    correction erreurs fonction evidence + ajout de ma partie

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -15,7 +15,7 @@ int nb_neighbours(game g, int num)
   //le compteur de voisins de num va incrémenter à chaque voisin vu
   int neighbours = 0;
   
-  for(int i = 0; i < game_nb_dirs(g); i++)
+  for(int i = 0; i < game_nb_dir(g); i++)
     {
       if(get_neighbour_dir(g, num, i) != -1)
 	neighbours++;
@@ -29,10 +29,10 @@ void evidence(game g)
   for(int i = 0; i < game_nb_nodes(g); i++)
     {
       //on regarde dans toutes les directions
-      for(int j = 0; j < game_nb_dirs(g); j++)
+      for(int j = 0; j < game_nb_dir(g); j++)
 	{
 	  //solution évidente 1: degré de l'île = nombre de voisins * nombre de ponts maximum
-	  if(get_required_degree(g, i) == nb_neighbours(g, i) * game_nb_max_bridges(g) && can_add_bridge_dir(g, i, j))
+	  if(get_required_degree(game_node(g,i)) == nb_neighbours(g, i) * game_nb_max_bridges(g) && can_add_bridge_dir(g, i, j))
 	    {
 	      for(int k = 0; k < game_nb_max_bridges(g); k++)
 	        add_bridge_dir(g, i, j);
@@ -108,11 +108,22 @@ game apply_possibility(int * tab, game g, int node_num){
   Plus de détail : https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
   Le probleme est les doublons générés lorsque deux nombres sont identiques...
 */
-static int * permutation (int * tab){
-  int * next_tab = malloc(sizeof(*tab));  
-  /*generate next_tab*/
+static int * next_possibility (int * tab, int n, int max){
+  int * next_tab = malloc(sizeof(int)*n);
+  int i = 0;
+  if (tab[n-1] >= max){
+    next_tab[0] = -1;
+    printf("coucou\n");
+    return next_tab;
+  }
+  while(tab[i] == max){
+    next_tab[i] = 0;
+    i++;
+  }
+  next_tab[i] = tab[i]+1;
   return next_tab;
 }
+
 
 /*
   Retourne une SList
@@ -122,19 +133,20 @@ static int * permutation (int * tab){
 SList create_possibilities(game g, int node_num){
   SList list = slist_create_empty();
   /* Générer toute les combinaisons possibles et les stocker dans la liste */
-
-  /*
-    Procédure :
-     - Comparer le degré requis avec le nombre de pont maximum.
-     - En fonction de ça faire une première répartition des points avec un degré maximum dans la répartition à 1, 2 ...
-     - Faire une permutation de cette combinaison pour obtenir toute les autres
-
-    Exemple : Si on a un degré requis de 4, un nombre de pont maximum à 2 et 4 directions possible sur 4 alors : 
-     -On a la combinaison [1 1 1 1] avec aucune variante. (en faite si avec la fonction permutation lexicographique...)
-     -La combinaison [2 1 1 0] et les variantes [1 2 1 0] , [0 1 2 1] ...
-*/
+  int n = game_nb_dir(g);
+  int max = game_nb_max_bridges(g);
+  int * tab = malloc(sizeof(int)*n);
+  for(int i = 0; i < n; i++){
+    tab[i] = 0;
+  }
+  while(tab[0] != -1){
+    list = slist_prepend(list, tab);
+    int * tab = next_possibility(tab, n, max);
+  }
+  printf("generation des possibilitées de %d terminée\n",node_num);
   return list;
 }
+
 
 /*
   Fonction principal elle effectue les appelles a toute les fonctions 
@@ -169,19 +181,28 @@ game solve (game g){
 }
 
 int main(int argc, char *argv[]) {
-  if( argc != 2){
-    printf("Aucun nom de fichier entré, veuillez écrire :\n./hashi_solve nom_du_fichier\n");
+  if( argc != 3){
+    printf("Erreur nombre d'argument");
     return EXIT_FAILURE;
   }
+  
   game g = translate_game(argv[1]);
-  if(solver_r(g,0,-1))
-     printf("solution found !\n");
-  else
-     printf("solution not found !\n");
-  if(game_over(g))
-     printf("ok\n");
-  else
-     printf("not ok\n");
+
+  if(atoi(argv[2]) == 1){
+    if(solver_r(g,0,-1))
+      printf("solution found !\n");
+    else
+      printf("solution not found !\n");
+    if(game_over(g))
+      printf("ok\n");
+    else
+      printf("not ok\n");
+  }
+
+  if(atoi(argv[2]) == 2){
+    g = solve(g);
+  }
+  
   char * save = malloc(sizeof(char)*100);
   sprintf(save, "%s.solved",argv[1]);
   write_save(g,save);
