a8b9a0161d03a8d4ae01fd06d4ba42c01abbbba1
camille <camille.meyrignac@etu.u-bordeaux.fr>
2017-03-09 10:29:55 +0100

    divers nettoyage

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -9,30 +9,23 @@
 #include "../core/file.h"
 
 bool solver_r(game g,int node_num,int dir,bool * go){
-  //si le nombre de ponts max d'une ile et atteint
   if(get_required_degree(game_node(g,node_num))==get_degree(g,node_num)){
-    //si on n'a pas parcouru chaque ile, on relance la récursivité avec la suivante
     if(node_num<(game_nb_nodes(g)-1)){
        solver_r(g,node_num+1,dir,go);
     }
-    //mais si le jeu est fini, on donne le résultat
     else{
       if(game_over(g)){
          *go= true;
          return go;
       }
-      //sinon quelque chose s'est mal passé, on supprime un pont
       else{
-         del_bridge_dir(g,node_num,dir); //revenir en arrière
+         del_bridge_dir(g,node_num,dir);
       }
     }
   }
-  //si le degré de l'ile vue n'est pas atteint
   else{
-    //on regarde dans toutes les directions
     for(int d=0; d<game_nb_dir(g);d++){
        if(can_add_bridge_dir(g,node_num,d)){
-	 //on peut poser un pont, on le pose et on relance la récursivité
         add_bridge_dir(g,node_num,d);
         solver_r(g,node_num,d,go);
       }
@@ -52,9 +45,9 @@ int main(int argc, char *argv[]) {
   }
   
   game g = translate_game(argv[1]);
+
   bool * go=malloc(sizeof(bool));
   *go=false;
-  
 
   if(solver_r(g,0,-1,go))
      printf("Solution trouvé!\n");
@@ -69,6 +62,7 @@ else {
    sprintf(save, "%s.solved",argv[1]);
    write_save(g,save);
 }
+free(go);
 delete_game(g);
 free(save);
 return EXIT_SUCCESS;
