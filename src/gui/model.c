905b679a92b910cb1f1fe5822df1144555827479
Adrien Chinour <adrienchinour@gmail.com>
2017-03-30 18:37:41 +0200

    gestion degré fini

--- src/gui/model.c
+++ src/gui/model.c
@@ -37,10 +37,29 @@ struct Env_t {
    int size; // taille d'une ile
 };
 
+void print_degree(int node_num, SDL_Renderer* ren,  Env * env){
+  SDL_Surface * surf;
+  SDL_Color color_menthe = { 22,184,78,255 }; //couleur menthe
+  SDL_Color color_corail = { 231,62,1,255 }; //couleur corail
+  node n = game_node(env->g, node_num);
+  TTF_Font * font = TTF_OpenFont(FONT, env->fontsize);
+  if(!font) ERROR("TTF_OpenFont: %s\n", FONT);
+  TTF_SetFontStyle(font, TTF_STYLE_BOLD);
+  char * degree = malloc(sizeof(char)*10);
+  sprintf(degree, "%d", get_required_degree(n));
+  if(get_required_degree(game_node(env->g, node_num)) == get_degree(env->g, node_num))
+    surf = TTF_RenderText_Blended(font, degree , color_menthe);
+  else
+    surf = TTF_RenderText_Blended(font, degree , color_corail);
+  env->text[node_num] = SDL_CreateTextureFromSurface(ren, surf);
+  SDL_FreeSurface(surf);
+  TTF_CloseFont(font);
+}
+
 /*
   Initialisation des variables d'env en fonction de la taille de l'écran
 */
-void init_window(int w, int h, Env * env){
+void init_window(int w, int h, SDL_Renderer* ren, Env * env){
   int max_x = 0; int max_y = 0;
   int margin_x = 0; int margin_y = 0;
 
@@ -65,7 +84,6 @@ void init_window(int w, int h, Env * env){
 
   // definition de la taille de la police
   env->fontsize = env->size/6;
-
 }
 
 static int coordtopxx(int coord, Env * env){
@@ -99,7 +117,7 @@ Env * init(SDL_Window* win, SDL_Renderer* ren, int argc, char* argv[]) {
   env->g = g;
   int w, h;
   SDL_GetWindowSize(win, &w, &h);
-  init_window(w,h,env);
+  init_window(w,h,ren,env);
 
   /* Init island texture from PNG image */
   env->island=malloc(sizeof(SDL_Texture*)*game_nb_nodes(g));
@@ -129,18 +147,7 @@ Env * init(SDL_Window* win, SDL_Renderer* ren, int argc, char* argv[]) {
   env->text = text;
 
   for(int i = 0 ; i < game_nb_nodes(g) ; i++){
-    node n = game_node(g, i);
-    SDL_Color color = { 231, 62, 1, 255 };
-    TTF_Font * font = TTF_OpenFont(FONT, env->fontsize);
-    if(!font) ERROR("TTF_OpenFont: %s\n", FONT);
-    TTF_SetFontStyle(font, TTF_STYLE_BOLD);
-    char * degree = malloc(sizeof(char)*10);
-    sprintf(degree, "%d", get_required_degree(n));
-    SDL_Surface * surf = TTF_RenderText_Blended(font, degree , color);// blended rendering for ultra nice text
-    //free(degree);
-    env->text[i] = SDL_CreateTextureFromSurface(ren, surf);
-    SDL_FreeSurface(surf);
-    TTF_CloseFont(font);
+    print_degree(i, ren, env);
   }
   env->node = -1;
   return env;
@@ -172,8 +179,8 @@ void render(SDL_Window* win, SDL_Renderer* ren, Env * env) {
 
       /* degree */
       SDL_QueryTexture(env->text[i], NULL, NULL, &rect.w, &rect.h);
-      rect.x = coordtopxx(get_x(n),env) + env->size / 2;
-      rect.y = coordtopxy(get_y(n),env) + env->size / 2;
+      rect.x = coordtopxx(get_x(n),env) + env->size /1.5;
+      rect.y = coordtopxy(get_y(n),env) + env->size /1.5;
       SDL_RenderCopy(ren, env->text[i], NULL, &rect);
 
       if(get_degree(env->g, i)){
@@ -266,26 +273,30 @@ int get_node(int x, int y, Env * env){
   return -1;
 }
 
-void make_connection(int node_num, Env * env){
+void make_connection(int node_num, SDL_Renderer * ren, Env * env){
   if(env->node != -1 && node_num != -1){
     for(int i = 0; i < game_nb_dir(env->g); i++){
       if(get_neighbour_dir(env->g, node_num, i) == env->node){
-	if(can_add_bridge_dir(env->g, node_num, i)){
-	  add_bridge_dir(env->g, node_num, i);
+        if(can_add_bridge_dir(env->g, node_num, i)){
+          add_bridge_dir(env->g, node_num, i);
           env->island[env->node] = env->islandnonselect;
           env->island[node_num]=env->islandnonselect;
-	}
-	else {
-	  while(get_degree_dir(env->g, node_num, i) != 0){
-             env->island[get_neighbour_dir(env->g, node_num, i)] = env->islandnonselect;
-             env->island[node_num]=env->islandnonselect;
-             del_bridge_dir(env->g, node_num, i);
-	  }
-	}
-	env->node = -1;
-	break;
+          print_degree(node_num, ren, env);
+        }
+        else {
+          while(get_degree_dir(env->g, node_num, i) != 0){
+            env->island[get_neighbour_dir(env->g, node_num, i)] = env->islandnonselect;
+            env->island[node_num]=env->islandnonselect;
+            del_bridge_dir(env->g, node_num, i);
+            print_degree(node_num, ren, env);
+          }
+        }
+        print_degree(env->node, ren ,env);
+        env->node = -1;
+        break;
       }
     }
+
   }
 }
 
@@ -303,7 +314,10 @@ bool process(SDL_Window* win, SDL_Renderer* ren, Env * env, SDL_Event * e) {
     switch (e->window.event) {
       case SDL_WINDOWEVENT_RESIZED:
         SDL_GetWindowSize(win, &width, &height);
-        init_window(width,height,env);
+        init_window(width,height,ren,env);
+        for(int i = 0; i < game_nb_nodes(env->g); i++){
+          print_degree(i, ren, env);
+        }
         break;
     }
   }
@@ -320,14 +334,14 @@ bool process(SDL_Window* win, SDL_Renderer* ren, Env * env, SDL_Event * e) {
        env->island[node_num] = env->islandnonselect;
     }
     else
-      make_connection(node_num, env);
+      make_connection(node_num, ren, env);
   }
 
   if(e->type == SDL_MOUSEBUTTONUP){
     SDL_Point mousedir;
     SDL_GetMouseState(&mousedir.x, &mousedir.y);
     int node_num = get_node(mousedir.x,mousedir.y, env);
-    make_connection(node_num, env);
+    make_connection(node_num, ren, env);
   }
 
   return false;
@@ -345,11 +359,11 @@ bool process(SDL_Window* win, SDL_Renderer* ren, Env * env, SDL_Event * e) {
       if(env->node == -1)
          env->node = node_num;
       else
-         make_connection(node_num, env);
+         make_connection(node_num, ren, env);
    }
    else if(e->type == SDL_FINGERUP){
       int node_num = get_node(tfinger.x,tfinger.y, env);
-      make_connection(node_num, env);
+      make_connection(node_num, ren, env);
    }
 
   #else
@@ -360,14 +374,14 @@ bool process(SDL_Window* win, SDL_Renderer* ren, Env * env, SDL_Event * e) {
       if(env->node == -1)
          env->node = node_num;
       else
-         make_connection(node_num, env);
+         make_connection(node_num, ren, env);
    }
 
    else if(e->type == SDL_MOUSEBUTTONUP){
       SDL_Point mousedir;
       SDL_GetMouseState(&mousedir.x, &mousedir.y);
       int node_num = get_node(mousedir.x,mousedir.y, env);
-      make_connection(node_num, env);
+      make_connection(node_num, ren, env);
    }
    #endif
 
